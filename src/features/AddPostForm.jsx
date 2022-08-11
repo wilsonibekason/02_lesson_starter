90e02284eb438fa6cb3c49804c58dc27123a5128
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-11 18:28:26 +0100

    complete add user content to blog post

--- src/features/AddPostForm.jsx
+++ src/features/AddPostForm.jsx
@@ -1,23 +1,36 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { selectAllUsers } from "./users/usersSlice";
 import { nanoid } from "@reduxjs/toolkit";
 import { postAdded } from "./posts/postSlice";
 const AddPostForm = () => {
   const dispatch = useDispatch();
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [userId, setuserId] = useState("");
 
   // handlechange
+  const users = useSelector(selectAllUsers);
   const ontitleChange = (e) => setTitle(e.target.value);
   const onContentChange = (e) => setContent(e.target.value);
+  const onAuthorChange = (e) => setuserId(e.target.value);
   const onSavePostClicked = (e) => {
     e.preventDefault();
     if (title && content) {
-      dispatch(postAdded(title, content));
+      dispatch(postAdded(title, content, userId));
       setTitle("");
       setContent("");
     }
   };
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+  const userOptions = users.map((user, index) => {
+    const { id, name } = user;
+    return (
+      <option value={id} key={id}>
+        {name}
+      </option>
+    );
+  });
   return (
     <>
       <section>
@@ -31,6 +44,16 @@ const AddPostForm = () => {
             value={title}
             onChange={ontitleChange}
           />
+          <label htmlFor="userContent">User Content</label>
+          <select
+            name=""
+            id="postAuthor"
+            value={userId}
+            onChange={onAuthorChange}
+          >
+            <option value=""></option>
+            {userOptions}{" "}
+          </select>
           <label htmlFor="postContent">Post Content</label>
           <input
             type="text"
@@ -39,7 +62,7 @@ const AddPostForm = () => {
             value={content}
             onChange={onContentChange}
           />
-          <button type="button" onClick={onSavePostClicked}>
+          <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
             save changes
           </button>
         </form>
