86e3793922b4ef6e30ae47112ef9b2c972a3548b
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-11 06:51:37 +0100

    complete add redux form

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = [
   { id: "1", title: "Learning redux toolkit", content: "i have good things" },
@@ -8,12 +8,23 @@ const postsSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
-    postAdded(state, action) {
-      state.push(action.payload);
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(title, content) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+          },
+        };
+      },
     },
   },
 });
-
+// produce a reducer and a prepare callback for our postAdded reducer function
 export const selectAllPosts = (state) => state.posts;
 export const { postAdded } = postsSlice.actions;
 export default postsSlice.reducer;
