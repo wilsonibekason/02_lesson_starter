a13cc678912cd03497e7c455a7d2eb4209522d56
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-12 05:03:12 +0100

    add reacton slice  and reducer function

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -7,6 +7,13 @@ const initialState = [
     title: "Learning redux toolkit",
     content: "i have good things",
     date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
 ];
 
@@ -26,13 +33,26 @@ const postsSlice = createSlice({
             content,
             userId,
             date: new Date().toISOString(),
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
           },
         };
       },
+    },
+    reactionsAdded(state, action) {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.find((post) => post.id === postId);
+      existingPost && existingPost.reactions[reaction]++;
+      //  [...existingPost, existingPost.reactions(reaction)++]
     },
   },
 });
 // produce a reducer and a prepare callback for our postAdded reducer function
 export const selectAllPosts = (state) => state.posts;
-export const { postAdded } = postsSlice.actions;
+export const { postAdded, reactionsAdded } = postsSlice.actions;
 export default postsSlice.reducer;
