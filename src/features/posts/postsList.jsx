18ff7e1bebd3b5a28973728db6f5dc0620ab4889
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-12 04:49:08 +0100

    sort postto be in order

--- src/features/posts/postsList.jsx
+++ src/features/posts/postsList.jsx
@@ -3,15 +3,21 @@ import { useSelector } from "react-redux";
 import React from "react";
 import { selectAllPosts } from "./postSlice";
 import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
+  // reverse postion of all post when created
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
   //?.subString(0, 100)
-  const renderPosts = posts.map((post) => (
+  const renderPosts = orderedPosts.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content}</p>
       <p className="postCredit">
         <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
       </p>
     </article>
   ));
