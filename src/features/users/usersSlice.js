f2dc17e90740b74cbf4228b6220f0a9281826346
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-16 19:02:58 +0100

    complete display users from api

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,10 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {
+  createSlice,
+  createImmutableStateInvariantMiddleware,
+  createAsyncThunk,
+} from "@reduxjs/toolkit";
+import axios from "axios";
+// create an axios instance and import as a base Url
+const USER_URL = "https://jsonplaceholder.typicode.com/users";
 
-const initialState = [{ id: "1", name: "wilson ibekason" }];
+// const initialState = [{ id: "1", name: "wilson ibekason" }];
+const initialState = {
+  users: [],
+};
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  try {
+    const response = await axios.get(USER_URL);
+    // return [...(await response).data]
+    return [...response.data];
+  } catch (error) {
+    return error.message;
+  }
+});
 const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    // addcase adddefaultcase addmatch
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      // meta type and payload
+      return action.payload;
+    });
+  },
 });
 
 export const selectAllUsers = (state) => state.users;
