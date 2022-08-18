df76480665dd1db1d81b1eb7141f6d1109a8c3f9
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-18 09:47:36 +0100

    using the usereducer hook in multiplex application

--- src/typescript/usereducer.tsx
+++ src/typescript/usereducer.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { INITIAL_STATE, postReducer } from './postReducer'
 // usereducer useage
 /**
  * 
@@ -6,9 +7,25 @@ import React from 'react'
  * when you updaye multiple states in a function
  */
 const usereducer = () => {
+    const [state, dispatch(name: string)] = useReducer(postReducer,INITIAL_STATE);
+
+    const handleFetch = () =>{
+        dispatch({type: "FETCH_START"});
+        fetch("json.placeholder.typicode.com/posts/1").then((res) => {
+            res.json()
+        }).catch((error) =>{
+            dispatch({type:})
+            console.log(error?.message);
+            
+        })
+    }
   return (
 <>
-
+<div>
+    <button onClick={handleFetch}>{state.loading ? "loading ... " : "fetch the post "}</button>
+    <p>{state.post?.title}</p>
+    <span>{state.error && "something went wrong"the}</span>
+</div>
 </>
     )
 }
