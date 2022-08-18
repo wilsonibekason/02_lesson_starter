1890da6a5cb4fc12058a02fb9b17d0d42a958ffb
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-18 06:37:47 +0100

    using the usereducer hook in multiplex application

---
+++ src/typescript/usereducer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// usereducer useage
+/**
+ * 
+ * @returns 
+ * when you updaye multiple states in a function
+ */
+const usereducer = () => {
+  return (
+<>
+
+</>
+    )
+}
+
+export default usereducer
